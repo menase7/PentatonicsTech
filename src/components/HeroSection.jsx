affb459561dfaf62e5d223efbedfd13d3aeb847e
Menase7 <menasedebel7@gmail.com>
2025-01-02 18:23:26 +0300

    hero section done

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function HeroSection() {
   return (
-    <div>HeroSection</div>
-  )
+    <div
+      className="lg:flex bg-cover bg-center h-fit w-full"
+      style={{ backgroundImage: 'url("/home.png")' }}
+    >
+      <div className="mx-auto max-w-[80%] space-y-5 pt-32">
+        <h1 className="text-white text-[36px] font-bold text-center">
+          Trusted Effective Service and Solutions
+        </h1>
+        <h3 className="text-[15px] text-white text-center">
+          It is very important to take care of the patient, the patient will be
+          followed by the patient, and at the same time they will experience
+          some great pain. Who had suspended him.
+        </h3>
+        <div className="flex justify-center gap-5 text-[14px] text-[#F5FAFF]">
+          <button className="h-[40px] w-[112px] bg-[#3D59FA] rounded-md hover:bg-[#F5FAFF] hover:text-[#051242] ease-in-out delay-100">
+            Contact Us
+          </button>
+          <button className="h-[40px] w-[112px] border-[#F5FAFF] border-2 rounded-md hover:bg-[#F5FAFF] hover:text-[#051242] ease-in-out delay-100">
+            Know More
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-[90%] mx-auto py-7">
+          <img className="w-full" src="/banner-img.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
