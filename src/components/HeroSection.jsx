1349aad1d95122d8909812f8f2e395a8310e0468
Menase7 <menasedebel7@gmail.com>
2025-01-03 21:20:04 +0300

    update on hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function HeroSection() {
   return (
     <div
-      className="lg:flex bg-cover bg-center h-fit w-full"
+      className="lg:grid lg:grid-cols-2 lg:items-center bg-cover bg-center h-fit lg:h-screen w-full"
       style={{ backgroundImage: 'url("/home.png")' }}
     >
-      <div className="mx-auto max-w-[80%] space-y-5 pt-32">
-        <h1 className="text-white text-[36px] font-bold text-center">
+      <div className="mx-auto max-w-[70%] space-y-5 lg:space-y-10 max-lg:pt-32">
+        <h1 className="text-white text-[36px] lg:text-[40px] font-bold text-center">
           Trusted Effective Service and Solutions
         </h1>
         <h3 className="text-[15px] text-white text-center">
@@ -15,7 +15,7 @@ export default function HeroSection() {
           followed by the patient, and at the same time they will experience
           some great pain. Who had suspended him.
         </h3>
-        <div className="flex justify-center gap-5 text-[14px] text-[#F5FAFF]">
+        <div className="flex justify-center lg:justify-start gap-5 text-[14px] text-[#F5FAFF]">
           <button className="h-[40px] w-[112px] bg-[#3D59FA] rounded-md hover:bg-[#F5FAFF] hover:text-[#051242] ease-in-out delay-100">
             Contact Us
           </button>
