2c4922af9d522342862c2d8c7a187157bc966c0b
Menase7 <menasedebel7@gmail.com>
2025-01-03 21:16:55 +0300

    slide down animation added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,15 @@ export default {
         sans: ['Open Sans', 'sans-serif'],
         catamaran: ['Catamaran', 'sans-serif'],
       },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+      },
+      animation: {
+        'slide-down': 'slideDown 0.3s ease-in-out',
+      },
     },
   },
   plugins: [],
