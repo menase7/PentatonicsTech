0798f9db6d44fc723fd1d8937b29e7f78d89573c
Menase7 <menasedebel7@gmail.com>
2025-01-04 22:49:18 +0300

    minor change for testing font

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function ServiceCard() {
   return (
-    <div>ServiceCard</div>
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-catamaran">Welcome to Tailwind CSS</h1>
+      <p className="text-lg font-sans mt-4">
+        This is a paragraph using the Open Sans font.
+      </p>
+    </div>
   )
 }
