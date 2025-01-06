001cb1e52ba27718c51b191164a3840fed3df485
Menase7 <menasedebel7@gmail.com>
2025-01-06 11:28:54 +0300

    serviceSection completed

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
-import img1 from "service1.png";
+import img1 from "../../public/service1.png";
 import img2 from "../../public/service2.png";
 import img3 from "../../public/service4.png";
 
 export default function ServiceSection() {
+  let services = [
+    {
+      img: "/service1.png",
+      title: "Visual Design",
+      description:
+        "The company itself is a very successful company. And even if it is the most to be assumed. elders, great",
+    },
+    {
+      img: "/service2.png",
+      title: "Development",
+      description:
+        "The company itself is a very successful company. And even if it is the most to be assumed. elders, great",
+    },
+    {
+      img: "/service4.png",
+      title: "It Management",
+      description:
+        "The company itself is a very successful company. And even if it is the most to be assumed. elders, great"
+    },
+  ]
+
   return (
-    <div>
-      <div className="text-center">
-        <p>Smart Service</p>
-        <h1>Provide All Kind Of Tech Solutions</h1>
-        <p className="w-[90%] mx-auto">
+    <div className="container mx-auto py-20">
+      <div className="text-center mb-[30px] lg:mb-[60px] max-w-[695px] mx-auto space-y-2">
+        <p className="text-[14px] text-[#3D59FA]">Smart Service</p>
+        <h1 className="text-[25px] md:text-[35px] lg:text-[40px] font-bold text-[#051242]">Provide All Kind Of Tech Solutions</h1>
+        <p className="w-[90%] mx-auto text-[15px] text-[#7A7E9A]">
           It is important to take care of the patient, to be followed by the
           patient, but it will happen at such a time that there is a lot of work
           and pain. Who had suspended him.
         </p>
       </div>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3">
-        {/* //img, title, description */}
-        <ServiceCard
-          img={img1}
-          title="Visual Design"
-          description="The company itself is a very successful company. And even if it is the most to be assumed. elders, great"
-        />
-        <ServiceCard
-          img={img2}
-          title="Development"
-          description="The company itself is a very successful company. And even if it is the most to be assumed. elders, great"
-        />
-        <ServiceCard
-          img={img3}
-          title="Development"
-          description="The company itself is a very successful company. And even if it is the most to be assumed. elders, great"
-        />
+        {
+          services.map((service, index)=>(
+            <ServiceCard key={index} img={service.img} title={service.title} description={service.description} />
+          ))
+        }
       </div>
     </div>
   );
