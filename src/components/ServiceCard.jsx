f5eda9c9b868f57afa92b1a22c385b60f501126b
Menase7 <menasedebel7@gmail.com>
2025-01-06 11:28:24 +0300

    serverCard completed

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,15 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function ServiceCard({img, title, description}) {
   return (
-    <div className="bg-green-600 mx-4 h-[100px]">
-      <div>
-        <img src={img} alt="img" />
+    <div className="shadow-md hover:bg-[#051242] text-[#051242] hover:text-white flex flex-col gap-3 justify-center items-center m-4 p-[20px] md:p-[30px] rounded-md">
+      <div className='h-[50px] lg:h-[60px] w-[50px] lg:w-[60px]'>
+        <Image width={60} height={60} src={img} alt='service' layout='responsive' />
       </div>
-      <p>{title}</p>
-      <p>
+      <p className='text-[21px] font-bold'>{title}</p>
+      <p className='text-center text-[#80839d] hover:text-[#c3c3c4]'>
         {description}
       </p>
+      <button className='text-[14px] font-semibold'>Read more</button>
     </div>
   )
 }
