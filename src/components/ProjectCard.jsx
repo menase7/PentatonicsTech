8f0c2f72064de4c4be03b4633b7c4cb336329067
Menase7 <menasedebel7@gmail.com>
2025-01-07 23:28:44 +0300

    project section and card added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function ProjectCard() {
+export default function ProjectCard({img, title, description}) {
   return (
-    <div>ProjectCard</div>
+    <div className="shadow-md flex flex-col gap-3 justify-center items-center m-4 p-[10px] md:p-[20px] rounded-md">
+      <div className='h-[50px] lg:h-[60px] w-[50px] lg:w-[60px]'>
+        <Image width={60} height={60} src={img} alt='service' layout='responsive' />
+      </div>
+      <p className='text-[21px] font-bold'>{title}</p>
+      <p className='text-center text-[#c3c3c4]'>
+        {description}
+      </p>
+    </div>
   )
 }
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 export default function ProjectSection() {
+  let services = [
+    {
+      img: "/service1.png",
+      title: "Research Product",
+      description:
+        "It is very important for the customer to be aware of the customer's needs, but this is what happens at the same time.",
+    },
+    {
+      img: "/service2.png",
+      title: "User Testing",
+      description:
+        "It is very important for the customer to be aware of the customer's needs, but this is what happens at the same time.",
+    },
+    {
+      img: "/service4.png",
+      title: "Development",
+      description:
+        "It is very important for the customer to be aware of the customer's needs, but this is what happens at the same time.",
+    },
+    {
+      img: "/service4.png",
+      title: "Product Handover",
+      description:
+        "It is very important for the customer to be aware of the customer's needs, but this is what happens at the same time.",
+    },
+  ];
+
   return (
-    <div>ProjectSection</div>
-  )
+    <div className="container bg-[#051242] text-white py-20 max-w-[1300px] mx-auto">
+      <div className="text-center mb-[30px] lg:mb-[60px] max-w-[695px] mx-auto space-y-2">
+        <p className="text-[14px]">Working Process</p>
+        <h1 className="text-[25px] md:text-[35px] lg:text-[40px] font-bold">
+          We are popular because of our way of working
+        </h1>
+        <p className="w-[90%] mx-auto text-[15px] text-[#bebfc7]">
+          It is important to take care of the patient, to be followed by the
+          client, but at the same time they will be affected by some great pains
+          and sufferings. Who had suspended him.
+        </p>
+      </div>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {services.map((service, index) => (
+          <ProjectCard
+            key={index}
+            img={service.img}
+            title={service.title}
+            description={service.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
