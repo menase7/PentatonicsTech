ef4ccc72b429d6c1eb731ac2233f8306215d00a8
Menase7 <menasedebel7@gmail.com>
2025-01-07 23:29:30 +0300

    changes on team card and team member

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,12 +1,29 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { FaFacebook, FaTelegram, FaTiktok, FaYoutube } from "react-icons/fa";
 
-export default function TeamCard({img, name, role}) {
+export default function TeamCard({ img, name, role }) {
   return (
-    <div className='bg-[#F5FAFF] my-3 w-fit flex flex-col pb-3 items-center justify-center'>
+    <div className="bg-[#F5FAFF] my-3 w-fit flex flex-col items-center justify-center group relative">
       <Image src={img} width={300} height={300} />
-      <h1 className='py-2 text-[21px] font-bold'>{name}</h1>
-      <p className=' text-[14px]'>{role}</p>
+      <div className="group-hover:bg-[#051242] text-[#051242] group-hover:text-white pb-3 w-full h-full">
+        <div className="absolute bottom-[86px] left-14 right-14 flex space-x-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+          <a className="bg-[#3D59FA] text-white p-2 rounded-md" href="">
+            <FaFacebook className="cursor-pointer" />
+          </a>
+          <a className="bg-[#3D59FA] text-white p-2 rounded-md" href="">
+            <FaTiktok className="cursor-pointer" />
+          </a>
+          <a className="bg-[#3D59FA] text-white p-2 rounded-md" href="">
+            <FaYoutube className="cursor-pointer" />
+          </a>
+          <a className="bg-[#3D59FA] text-white p-2 rounded-md" href="">
+            <FaTelegram className="cursor-pointer" />
+          </a>
+        </div>
+        <h1 className="py-2 text-[21px] font-bold">{name}</h1>
+        <p className="text-[14px] text-[#8689a3] group-hover:text-[#c0c0c3]">{role}</p>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -26,19 +26,17 @@ export default function TeamMember() {
     }
   ]
   return (
-    <div>
-      <div>
+    <div className="text-center mt-10">
+      <div className="space-y-3">
         <h2 className="text-[14px] text-[#3D59FA]">Team Members</h2>
         <h1 className="text-[25px] md:text-[35px] lg:text-[40px] font-bold text-[#051242]">People Who Are Behind The Achievements</h1>
-        <p className="w-[60%] mx-auto text-[15px] text-[#7A7E9A]">
+        <p className="w-[80%] mx-auto text-[15px] text-[#7A7E9A]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
           repellendus tempora odit ipsum, exercitationem possimus voluptate hic
           doloremque provident expedita suscipit sint!
         </p>
       </div>
-      {/* slider */}
-
-      <div className="flex w-[80%] mx-auto gap-3 overflow-auto">
+      <div className="flex w-[80%] mx-auto gap-3">
         {
           teams.map((team, index)=>(
             <TeamCard key={index} img={team.img} name={team.name} role={team.role} />
