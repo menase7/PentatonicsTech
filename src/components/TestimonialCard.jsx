468142681b157a59ca74e58abf303b6e03b1049a
Menase7 <menasedebel7@gmail.com>
2025-01-09 14:37:46 +0300

    testimonialCard added

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-export default function TestimonialCard() {
+export default function TestimonialCard({ img, name, role, review }) {
   return (
-    <div>TestimonialCard</div>
-  )
+    <div className="bg-white max-h-[400px] flex flex-col items-center justify-center group relative shadow-md p-4 mx-5">
+      <div className="size-[60px]">
+        <Image src={img} width={100} height={100} alt={name} />
+      </div>
+      <div className="text-[#8689a3] w-[90%] text-center mt-4">
+        {review}
+      </div>
+      <div className="text-center mt-4">
+        <p className="py-2 text-[21px] font-bold">{name}</p>
+        <p className="text-[14px] text-[#8689a3]">{role}</p>
+      </div>
+    </div>
+  );
 }
