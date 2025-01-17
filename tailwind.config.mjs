db6cff87944ddd446e09e3fd9aae9ae635999665
Menase7 <menasedebel7@gmail.com>
2025-01-17 21:48:53 +0300

    triangle and rectangle animations added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,12 +17,32 @@ export default {
       },
       keyframes: {
         slideDown: {
-          '0%': { transform: 'translateY(-100%)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 },
+          "0%": { transform: "translateY(-100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        trianglePath: {
+          "0%": { transform: "translate(0, 0)" },
+          "33%": { transform: "translate(87px, -70px)" },
+          "66%": { transform: "translate(-87px, -70px)" },
+          "100%": { transform: "translate(0, 0)" },
+        },
+        rectanglePath: {
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(87px, -70px)" },
+          "50%": { transform: "translate(-87px, -70px)" },
+          "75%": { transform: "translate(-87px, 70px)" },
+          "100%": { transform: "translate(0, 0)" },
         },
       },
       animation: {
-        'slide-down': 'slideDown 0.3s ease-in-out',
+        "slide-down": "slideDown 0.3s ease-in-out",
+        "rotate-infinite": "rotate 9s linear infinite",
+        "triangle-move": "trianglePath 12s linear infinite",
+        "rectangle-move": "rectanglePath 12s linear infinite"
       },
     },
   },
